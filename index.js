b245cab513a92a92b67a617f190179fe466d78f7
Danait Semere <semeregdanait@gmail.com>
2025-04-10 14:28:48 +0300

    added a js file

--- index.js
+++ index.js
@@ -1,56 +1,53 @@
+function lengthOfItems(names) {
+  return names.map(array=> array.length);
+}
 
-function getStringLengths(strings) {
-    return strings.map(str => str.length);
-  }
+const arrayOfname= ["abera", "abebe", "Mary","shallom","Freta"];
+console.log(lengthOfItems(arrayOfname));
 
-  const stringArray = ["apple", "banana", "cherry"];
-  const lengths = getStringLengths(stringArray);
-  console.log(lengths); 
-  function filterEvenNumbers(numbers) {
-    return numbers.filter(num => num % 2 === 0);
-  }
-  
+function wholeNumbers(number) {
+  return number
+  .filter(i => i % 2 === 0);
+}
 
-  const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const evenNumbers = filterEvenNumbers(numberArray);
-  console.log(evenNumbers); 
-  
-  
-  function getTopStudents(students) {
-    return students
-      .filter(student => student.score > 75)
-      .map(student => student.name);
-  }
-  
-  
-  const studentArray = [
-    { name: "Alice", score: 85 },
-    { name: "Bob", score: 70 },
-    { name: "Charlie", score: 92 },
-    { name: "David", score: 68 },
-    { name: "Eve", score: 78 },
-  ];
-  const topStudents = getTopStudents(studentArray);
-  console.log(topStudents); 
-  
-  
-  function squareNumbers(numbers) {
-    return numbers.map(num => num * num);
-  }
-  
+const evenNum= [20,27,47,22,69,90,87,65,80];
+console.log(wholeNumbers(evenNum));
 
-  const numbersToSquare = [1, 2, 3, 4, 5];
-  const squaredNumbers = squareNumbers(numbersToSquare);
-  console.log(squaredNumbers); 
-  
-  /
-  function processMixedArray(mixedArray) {
-    return mixedArray
-      .filter(item => typeof item === 'string')
-      .map(str => str.toUpperCase());
-  }
-  
-  
-  const mixedData = [10, "hello", true, "world", 25, "javascript"];
-  const processedStrings = processMixedArray(mixedData);
-  console.log(processedStrings);
+
+
+function gradeOfStudents(students) {
+  const gradeOfStudents = students.filter(name => name.grade > 75);
+  return gradeOfStudents;
+}
+
+const students = [
+  { name: "barry", grade: 50 },
+  { name: "Lwi", grade: 60 },
+  { name: "Fkrte", grade: 90 },
+  { name: "Aman", grade: 75 },
+  { name: "Berry", grade: 89 },
+
+];
+
+console.log(gradeOfStudents(students));
+
+
+
+
+function squareNumbers(num) {
+  return num.map(i => i** 2);
+}
+
+const sqNumber= [20, 44, 58, 69];
+console.log(squareNumbers(sqNumber));
+
+
+
+function mixedArray(item) {
+  return item
+      .filter(i => typeof i === "string")
+      .map(j => j.toUpperCase());
+}
+
+const strNumChar = ["Abebe", 32, "Berhe", 72, "*", "%","Maranata",72.7];
+console.log(mixedArray(strNumChar));
